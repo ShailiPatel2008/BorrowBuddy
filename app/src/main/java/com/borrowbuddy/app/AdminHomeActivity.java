fa72db55a3af9e42ec669f4a787f4ba5b63b50e9
Fency Patel <fency2807@gmail.com>
2026-08-16 19:33:45 +0530

    Add BorrowBuddy Android project

---
+++ app/src/main/java/com/borrowbuddy/app/AdminHomeActivity.java
@@ -0,0 +1,15 @@
+package com.borrowbuddy.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_admin_home);
+    }
+}
